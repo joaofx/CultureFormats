f3f39f3e22b362c2da2e1a76b40cc6113278507c
joaofx <joaofx@gmail.com>
2016-08-23 15:14:42 +0100

    First commit

---
+++ CultureFormats/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Globalization;
+using System.Web.Mvc;
+
+namespace CultureFormats.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index(string culture = "")
+        {
+            return View(
+                string.IsNullOrEmpty(culture) ? 
+                    CultureInfo.InvariantCulture : 
+                    CultureInfo.GetCultureInfo(culture));
+        }
+    }
+}
